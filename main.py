642c711694ecade89a1fa8f86eace45a0447639c
Agstarte <aga1999.1@gmail.com>
2021-01-03 17:03:36 +0100

    proste łączenie się z bazą danych

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Start")
+import mysql.connector
+
+import mysql.connector
+
+'''connecting to local database'''
+database = mysql.connector.connect(
+    host="localhost",
+    user="user",
+    password="123",
+    database="wypozyczalnia_kostiumow"
+)
+
+# print(database)
+
+cursor = database.cursor()
+
+# my_cursor.execute("SHOW TABLES")
+# for x in my_cursor:
+#   print(x)
+
+cursor.execute("SELECT * FROM klient")
+clients = cursor.fetchall()
+for client in clients:
+    print(client)
