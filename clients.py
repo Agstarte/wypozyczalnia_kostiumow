bbb1bc44e47a927b5fbaaba2364d144a71a168ed
Agstarte <aga1999.1@gmail.com>
2021-01-04 18:25:41 +0100

    wyświtlanie, dodawanie oraz edytowanie klienta

--- clients.py
+++ clients.py
@@ -124,7 +124,74 @@ def clients(database, root):
     def edit_function():
         edit_client = Toplevel()
         edit_client.title("Modyfikuj dane klienta")
-        edit_client.geometry(f"350x350+{top.winfo_x()}+{top.winfo_y()}")
+        edit_client.geometry(f"350x250+{top.winfo_x()}+{top.winfo_y() + 50}")
+
+        blank = Label(edit_client)
+        blank.grid(row=0, column=1)
+        blank = Label(edit_client, width=6)
+        blank.grid(row=0, column=0)
+        Label(edit_client, text="Podaj identyfikator klienta").grid(row=1, column=1, columnspan=2)
+        client_id = Entry(edit_client, width=20)
+        client_id.grid(row=2, column=1, columnspan=2)
+
+        dane = []
+        columns = ["id_osoby", "imię", "nazwisko", "numer telefonu", "e-mail"]
+        for i in range(5):
+            e = tk.Entry(edit_client, disabledforeground="black")
+            e.grid(row=6 + i, column=1)
+            e.insert(END, columns[i])
+            e.config(state='disabled')
+            e = tk.Entry(edit_client, disabledforeground="black")
+            e.grid(row=6 + i, column=2)
+            e.insert(END, '')
+            e.config(state='disabled')
+            dane.append(e)
+
+        def show():
+            try:
+                cursor = database.cursor()
+                cursor.execute(f"SELECT * FROM klient WHERE id_osoby = {client_id.get()}")
+                client = cursor.fetchall()
+
+                if str(client[0][0]) == 'None':
+                    return
+                for i in range(5):
+                    dane[i].config(state='normal')
+                    dane[i].insert(END, str(client[0][i]))
+                update_button.configure(state=NORMAL)
+            except:
+                messagebox.showerror("Błąd", "Wprowadzono niepoprawny identyfikator.")
+
+        def update():
+            cursor = database.cursor()
+            # cursor.execute(f"SELECT * FROM klient WHERE id_osoby = {client_id.get()}")
+            # client = cursor.fetchall()
+            print(str(dane[0].get()) + '//' + str(dane[1].get()) + '//' + str(dane[2].get()) + '//' + str(dane[3].get()) + '//' + str(dane[4].get()))
+            if str(dane[0].get()) == '' or str(dane[1].get()) == '' or str(dane[2].get()) == '' or str(dane[3].get()) == '':
+                messagebox.showerror("Błąd", "Identyfikator, Imię, Nazwisko i numer telefonu nie mogą być puste!")
+                return
+            try:
+                cursor.execute(f"UPDATE klient SET id_osoby = {dane[0].get()}, imie = '{dane[1].get()}',"
+                               f" nazwisko = '{dane[2].get()}', nr_tel = '{dane[3].get()}', email = '{dane[4].get()}' "
+                               f"WHERE id_osoby = {client_id.get()}")
+            except Exception as e:
+                messagebox.showerror("Błąd", e)
+                return
+            messagebox.showinfo("Informacja", "Pomyślnie zmieniono dane klienta")
+            database.commit()
+
+        blank = Label(edit_client)
+        blank.grid(row=3, column=1)
+        Button(edit_client, text="Pokaż", fg="black", bg="#bfa7a8", width=10, command=show).grid(row=4, column=1)
+        blank = Label(edit_client)
+        blank.grid(row=5, column=1)
+
+        update_button = Button(edit_client, text="Aktualizuj", fg="black", bg="#bfa7a8", width=10, command=update)
+        update_button.grid(row=4, column=2)
+        update_button.configure(state=DISABLED)
+
+
+        edit_client.mainloop()
 
     def exit_function():
         top.destroy()
@@ -163,4 +230,5 @@ def clients(database, root):
     exit_button = tk.Button(top, text="Powrót", padx=30, pady=5, fg="black", bg="#bfa7a8",
                                     command=exit_function)
     exit_button.pack()
+    top.protocol("WM_DELETE_WINDOW", exit_function)
     top.mainloop()
