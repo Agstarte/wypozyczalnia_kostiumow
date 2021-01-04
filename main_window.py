b18110511c064569af14cd4b8d59478526ef2bd3
Agstarte <aga1999.1@gmail.com>
2021-01-04 13:59:34 +0100

    manu

--- main_window.py
+++ main_window.py
@@ -1,9 +1,9 @@
 import tkinter as tk
+from tkinter import *
 from tkinter import Text
 import os
 import mysql.connector
 import clients
-
 
 
 def main_window(database, root):
@@ -18,19 +18,45 @@ def main_window(database, root):
     y = (hs / 2) - (h / 2)
     root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-
     # canvas = tk.Canvas(root, height=350, width=350, bg="#c3a7a8")
     # canvas.pack()
 
-
     def clients_function():
-        print("You clicked button1")
         clients.clients(database, root)
 
-    text = tk.Label(root, text='tutaj tekst', padx=50, pady=5)
-    text.grid(row=0, column=2)
+    def products_function():
+        print("Zarządzanie produktami")
+        # clients.clients(database, root)
 
-    clients_button = tk.Button(root, text="Zarządzanie klientami", padx=10, pady=5, fg="black", bg="#bfa7a8", command=clients_function)
-    clients_button.grid(row=2, column=2)
+    def reservations_function():
+        print("Zarządzanie rezerwacjami")
+        # clients.clients(database, root)
+
+    blank = Label(root)
+    blank.pack()
+    blank = Label(root)
+    blank.pack()
+    blank = Label(root, text="Menu")
+    blank.pack()
+    blank = Label(root)
+    blank.pack()
+
+    clients_button = tk.Button(root, text="Zarządzanie klientami", padx=10, pady=5, fg="black", bg="#bfa7a8",
+                               command=clients_function)
+    clients_button.pack()
+
+    blank = Label(root)
+    blank.pack()
+
+    products_button = tk.Button(root, text="Zarządzanie klientami", padx=10, pady=5, fg="black", bg="#bfa7a8",
+                                command=products_function)
+    products_button.pack()
+
+    blank = Label(root)
+    blank.pack()
+
+    reservations_button = tk.Button(root, text="Zarządzanie klientami", padx=10, pady=5, fg="black", bg="#bfa7a8",
+                                    command=reservations_function)
+    reservations_button.pack()
 
     root.mainloop()
