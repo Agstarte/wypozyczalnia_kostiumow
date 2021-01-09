3a77387ce8d13da55aceb09f39554af74575d25d
Miron <hamer223@o2.pl>
2021-01-09 20:34:54 +0100

    added show_function

--- reservations.py
+++ reservations.py
@@ -17,22 +17,23 @@ def reservations(database, root):
 
     def show_function():
         show_reservation = Toplevel()
-        show_reservation.title("Wyświetl klienta")
+        show_reservation.title("Wyświetl rezerwację")
         show_reservation.geometry(f"350x250+{top.winfo_x()}+{top.winfo_y() + 50}")
 
         blank = Label(show_reservation)
         blank.grid(row=0, column=1)
         blank = Label(show_reservation, width=6)
         blank.grid(row=0, column=0)
-        Label(show_reservation, text="Podaj identyfikator klienta").grid(row=1, column=1, columnspan=2)
-        client_id = Entry(show_reservation, width=20)
-        client_id.grid(row=2, column=1, columnspan=2)
+        Label(show_reservation, text="Podaj identyfikator rezerwacji").grid(row=1, column=1, columnspan=2)
+        reservation_id = Entry(show_reservation, width=20)
+        reservation_id.grid(row=2, column=1, columnspan=2)
 
-        columns = ["id_osoby", "imię", "nazwisko", "numer telefonu", "e-mail"]
-        for i in range(5):
+        row_count = 6
+        rows = ["id_rezerwacji", "id_osoby", "data_poczatku_rezerwacji", "data_konca_rezerwacji", "cena", "status"]
+        for i in range(row_count):
             e = tk.Entry(show_reservation, disabledforeground="black")
             e.grid(row=6 + i, column=1)
-            e.insert(END, columns[i])
+            e.insert(END, rows[i])
             e.config(state='disabled')
             e = tk.Entry(show_reservation, disabledforeground="black")
             e.grid(row=6 + i, column=2)
@@ -42,13 +43,13 @@ def reservations(database, root):
         def show():
             try:
                 cursor = database.cursor()
-                cursor.execute(f"SELECT * FROM klient WHERE id_osoby = {client_id.get()}")
+                cursor.execute(f"SELECT * FROM rezerwacja WHERE id_rezerwacji = {reservation_id.get()}")
                 client = cursor.fetchall()
 
                 if str(client[0][0]) == 'None':
                     return
 
-                for i in range(5):
+                for i in range(row_count):
                     e = tk.Entry(show_reservation, disabledforeground="black")
                     e.grid(row=6 + i, column=2)
                     e.insert(END, str(client[0][i]))
